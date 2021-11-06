8a7cc22e1d250d70321e99a8ddafd5931d57fe87
s.ansun@yahoo.com <ansunsujoe@gmail.com>
2021-11-06 09:09:40 -0500

    Metrics

--- swift/metrics.sh
+++ swift/metrics.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-ls /srv/node/sdb/objects | wc -l
+if [ $1 == "dataloc" ]; then
+    ssh root@$2 'find /srv/node/sdb/objects -name *.data | xargs head -n3' | grep oid
+elif [ $1 == "datacount" ]; then
+    ssh root@$2 'find /srv/node/sdb/objects -name *.data | wc -l'
+fi
