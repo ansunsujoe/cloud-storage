7e02876d24b8c152c9d85b54dac2a2885557356f
s.ansun@yahoo.com <ansunsujoe@gmail.com>
2021-11-19 08:52:03 -0600

    Debug test

--- swift/swiftapi.py
+++ swift/swiftapi.py
@@ -194,6 +194,7 @@ class SwiftClient():
         
     def get_data_movement_stats_v2(self):
         for ip in self.ring_conf.get("storage_nodes"):
+            print("hello")
             try:
                 result = subprocess.check_output(["./stats.sh", "object-requests", ip, "PUT /v1", self.last_event_time], 
                                                     universal_newlines=True, 
