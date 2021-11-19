2a1ebb4f53433952087426821fa0551b7584bae5
s.ansun@yahoo.com <ansunsujoe@gmail.com>
2021-11-19 08:54:48 -0600

    Last event time

--- swift/swiftapi.py
+++ swift/swiftapi.py
@@ -194,8 +194,8 @@ class SwiftClient():
         
     def get_data_movement_stats_v2(self):
         for ip in self.ring_conf.get("storage_nodes"):
-            print("hello")
-            result = subprocess.check_output(["./stats.sh", "object-requests", ip, "PUT /v1", self.last_event_time], 
+            last_event_time = self.last_event_time if self.last_event_time is not None else "None"
+            result = subprocess.check_output(["./stats.sh", "object-requests", ip, "PUT /v1", last_event_time], 
                                                 universal_newlines=True, 
                                                 timeout=3).strip()
             print(result)
