d709d3b15c92d5698a06495a65308a8ef94b4d01
s.ansun@yahoo.com <ansunsujoe@gmail.com>
2021-12-11 17:27:42 -0600

    Swish

--- swift/swiftapi.py
+++ swift/swiftapi.py
@@ -10,6 +10,8 @@ import time
 from tqdm import tqdm
 import os
 from fabric import Connection
+import threading
+from queue import Queue
 
 def moving_average(array, interval):
     if len(array) < interval:
@@ -77,6 +79,11 @@ class SwiftClient:
         with open("../vmconfig.json", "r") as f:
             self.vm_names = json.load(f)
             
+        # Create the storage nodes and cluster objects
+        self.cluster = StorageCluster()
+        for ip in self.vm_names.get("cluster_nodes"):
+            self.cluster.add(StorageNode(ip, 100, "running"))
+            
         # Set up Swift Credentials
         self.add_auth_variables()
         
@@ -86,7 +93,7 @@ class SwiftClient:
         print("Successful initialization!")
         
         # Set up logreader
-        self.lr = LogReader()
+        self.lr = LogReader(host="192.168.1.99")
 
     def initconfig(self):
         for ip in self.ring_conf.get("storage_nodes"):
@@ -103,31 +110,28 @@ class SwiftClient:
         # Account builder
         account_replicas = self.ring_conf.get("account").get("replicas")
         account_ips = self.ring_conf.get("account").get("hosts")
-        account_weights = self.ring_conf.get("account").get("weights")
         subprocess.run(["swift-ring-builder", "account.builder", "create", "10", str(account_replicas), "1"])
         for i in range(len(account_ips)):
             subprocess.run(["swift-ring-builder", "account.builder", "add", "--region", "1", "--zone", "1",
-                            "--ip", account_ips[i], "--port", "6202", "--device", "sdb", "--weight", str(account_weights[i])])
+                            "--ip", account_ips[i], "--port", "6202", "--device", "sdb", "--weight", "100"])
         subprocess.run(["swift-ring-builder", "account.builder", "rebalance"])
         
         # Container builder
         container_replicas = self.ring_conf.get("container").get("replicas")
         container_ips = self.ring_conf.get("container").get("hosts")
-        container_weights = self.ring_conf.get("container").get("weights")
         subprocess.run(["swift-ring-builder", "container.builder", "create", "10", str(container_replicas), "1"])
         for i in range(len(container_ips)):
             subprocess.run(["swift-ring-builder", "container.builder", "add", "--region", "1", "--zone", "1",
-                            "--ip", container_ips[i], "--port", "6201", "--device", "sdb", "--weight", str(container_weights[i])])
+                            "--ip", container_ips[i], "--port", "6201", "--device", "sdb", "--weight", "100"])
         subprocess.run(["swift-ring-builder", "container.builder", "rebalance"])
         
         # Object builder
         object_replicas = self.ring_conf.get("object").get("replicas")
         object_ips = self.ring_conf.get("object").get("hosts")
-        object_weights = self.ring_conf.get("object").get("weights")
         subprocess.run(["swift-ring-builder", "object.builder", "create", "10", str(object_replicas), "1"])
         for i in range(len(object_ips)):
             subprocess.run(["swift-ring-builder", "object.builder", "add", "--region", "1", "--zone", "1",
-                            "--ip", object_ips[i], "--port", "6200", "--device", "sdb", "--weight", str(object_weights[i])])
+                            "--ip", object_ips[i], "--port", "6200", "--device", "sdb", "--weight", "100"])
         subprocess.run(["swift-ring-builder", "object.builder", "rebalance"])
         
         # Move gz files to correct place
@@ -466,8 +470,8 @@ if __name__ == "__main__":
     client.restart_nodes()
     
 class LogReader:
-    def __init__(self):
-        self.c = Connection(host="192.168.1.99", user="root")
+    def __init__(self, ip):
+        self.c = Connection(host=ip, user="root")
         self.last_read_time = None
         
     def read(self):
@@ -475,10 +479,11 @@ class LogReader:
             result = self.c.run(f"journalctl -u openstack-swift-object --since '{self.last_read_time}' | grep PUT").stdout
         else:
             result = self.c.run(f"journalctl -u openstack-swift-object | grep PUT").stdout
-        put_requests = [entry for entry in result.split("\n") if "PUT /sdb" in entry]
+        return [entry for entry in result.split("\n") if "PUT /sdb" in entry]
+            
+    def process_puts(self, put_requests):
         for entry in put_requests:
             request_array = entry.split()
-            last_time = request_array[0]
             ts = request_array[2]
             object_url = request_array[11][:-1].split("/")[-1]
             if not object_url.startswith("stock-data"):
@@ -490,7 +495,7 @@ class LogReader:
                                                 timeout=3, 
                                                 stderr=subprocess.DEVNULL).strip().split()[4])
             response_time = float(request_array[19])
-            print(f"PUT Time: {ts}, Object: {object_url}, Object Size: {object_size}, Time: {response_time}, {last_time}")
+            print(f"PUT Time: {ts}, Object: {object_url}, Object Size: {object_size}, Time: {response_time}")
         
         
 class StorageNode:
@@ -498,6 +503,7 @@ class StorageNode:
         self.ip = ip
         self.weight = weight
         self.status = "Running"
+        self.lr = LogReader(self.ip)
         
     def startup(self):
         pass
@@ -506,11 +512,25 @@ class StorageNode:
         pass
         
 class StorageCluster:
-    def __init__(self, storage_nodes):
-        self.nodes = storage_nodes
+    def __init__(self):
+        self.nodes = []
         
-    def drop_nodes(self, num_nodes):
+    def add(self, node):
+        self.nodes.append(node)
+        
+    def shutdown_nodes(self, num_nodes):
         pass
     
-    def add_nodes(self, num_nodes):
+    def restart_nodes(self, num_nodes):
         pass
+    
+    def __repr__(self):
+        # Stats logging
+        t = PrettyTable(["Node IP", "Num Objects"])
+        for ip in self.ring_conf.get("storage_nodes"):
+            try:
+                result = subprocess.check_output(["./stats.sh", "datacount", ip], universal_newlines=True, 
+                                                 timeout=3, stderr=subprocess.DEVNULL).strip()
+                t.add_row([ip, result])
+            except Exception:
+                t.add_row([ip, 0])
